db0383d6bfa3e7778ee06fa8701e8cf4fc123d41
Jerônimo Nunes Rocha <jeronimonunes@live.com>
2019-10-19 02:20:30 -0300

    Listing possible datasets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { map, shareReplay, startWith } from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
+import { filter } from 'minimatch';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'earth-timelapse';
+
+  what = new FormControl('');
+
+  // tslint:disable-next-line: max-line-length
+  allLayers$ = this.httpClient.get('https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0', { responseType: 'text' })
+    .pipe(
+      map(text => new DOMParser().parseFromString(text, 'text/xml')),
+      map(xml => xml.querySelectorAll('Capability>Layer>Layer')),
+      map(nodeList => Array.from(nodeList)),
+      shareReplay(1)
+    );
+
+  options$ = combineLatest([this.allLayers$, this.what.valueChanges.pipe(startWith(this.what.value))])
+    .pipe(
+      map(([layers, text]) => layers.filter(layer => {
+        const title = layer.querySelector('Title')!.innerHTML;
+        return title.toLocaleLowerCase().indexOf(text.toLocaleLowerCase()) !== -1;
+      }))
+    );
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
